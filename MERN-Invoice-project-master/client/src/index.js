bd368211954ec20d66653adae60a28cc14a85af8
“Shweta” <b220033@nitsikkim.ac.in>
2025-04-26 21:15:44 +0530

    feat(index): update index.js to set up app initialization and rendering

---
+++ MERN-Invoice-project-master/client/src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import "./index.css";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+if (process.env.NODE_ENV === "production") {
+	disableReactDevTools();
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<Router>
+				<Routes>
+					<Route path="/*" element={<App />} />
+				</Routes>
+			</Router>
+		</Provider>
+	</React.StrictMode>
+);
+
+reportWebVitals();
