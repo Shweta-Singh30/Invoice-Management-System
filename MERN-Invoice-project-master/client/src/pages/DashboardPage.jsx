c7bc9c6aef532f09c6de086c0ea5efc54dd19d42
“Shweta” <b220033@nitsikkim.ac.in>
2025-04-26 21:11:39 +0530

    Added Dashboard and Home page

---
+++ MERN-Invoice-project-master/client/src/pages/DashboardPage.jsx
@@ -0,0 +1,18 @@
+import{Box,Typography} from "@mui/material"
+
+const DashboardPage =() =>{
+    return(
+        <Box
+        sx={{
+            display: "flex",
+            marginLeft: "250px",
+            mt: 10
+        }}
+        >
+            <Typography variant="h3"> This Dashboard page is only allow to logged in users
+            </Typography>
+        </Box>
+    );
+};
+
+export default DashboardPage;
